dd51331f923d3a5327f4de4230756c3a2d7fddc9
James Do <dojames3@yahoo.com>
2022-01-24 16:50:33 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, {Fragment} from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing"
-const App = () => 
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import {Provider} from 'react-redux';
+import store from './store'
+import './App.css';
 
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Landing/>} />
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
+);
 export default App;
