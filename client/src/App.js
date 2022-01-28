f6f98560736aad4a342e2ebfca7040513258f643
James Do <dojames3@yahoo.com>
2022-01-28 16:45:19 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import {Provider} from 'react-redux';
 import store from './store';
 import {loadUser} from './actions/auth';
@@ -35,6 +39,22 @@ const App = () => {
           <Route
             path="dashboard"
             element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path="create-profile"
+            element={<PrivateRoute component={CreateProfile} />}
+          />
+          <Route
+            path="edit-profile"
+            element={<PrivateRoute component={EditProfile} />}
+          />
+          <Route
+            path="add-experience"
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path="add-education"
+            element={<PrivateRoute component={AddEducation} />}
           />
         </Routes>
       </Fragment>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Fragment} from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {getCurrentProfile} from '../../actions/profile'
+import {getCurrentProfile} from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
+const Dashboard = ({getCurrentProfile, auth:{user}, profile: {profile, loading}}) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-  return <div>
-      Dashboard
-  </div>;
+  return loading && profile === null ? <Spinner/> : <section className="container">
+      <h1 className="large text-primary">
+          Dashboard
+      </h1>
+      <p className="lead">
+          <i className="fas fa-user">Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (<section>
+          <DashboardActions/>
+          <Experience experience={profile.experience}/>
+          <Education education={profile.education}/>
+          </section>): 
+      (<section className="container">
+        <p>
+            You have not yet setup a profile, please add more info
+        </p>
+        <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link>
+      </section>)}
+  </section>
 };
 
 Dashboard.propTypes = {
